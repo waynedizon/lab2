4e2a5e2d1733b9f9c288c3c25a8aab90080432f4
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-01-14 19:33:31 +0800

    W3 Schools Javascript Part 1

--- week5/script.js
+++ week5/script.js
@@ -1,5 +1,6 @@
-alert("Welcome");
+alert("Welcome"); //pop up on the home page
 
+//get text from index.html
 let text = `Hi there! I enjoy capturing memories to cherish later, <br> and I encourage you to do the same.`;
 document.getElementById("js1").innerHTML = text;
 
