d4e8c71d761fed5f3aef7b42cb57f9d274205986
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-01-22 21:12:08 +0800

    W3 Schools Javascript Lesson 20. Classes

--- week5/script.js
+++ week5/script.js
@@ -14,4 +14,19 @@ window.addEventListener("load", wayne);
 // A button object calls the function:
 document.getElementById("btn").addEventListener("click", wayne);
 
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age(x) {
+    return x - this.year;
+  }
+}
 
+const date = new Date();
+let currentYear = date.getFullYear();
+
+const myCar = new Car("Pink Lamborghini Aventador", 2023);
+document.getElementById("demo").innerHTML =
+"My name is Wayne, and my " + myCar.name + " is " + myCar.age(currentYear) + " years old.";
