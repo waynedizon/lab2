ed81b9595180532c9f4f192d7f942fe10a647063
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-01-23 09:08:47 +0800

    Week 8 PHP

---
+++ week8/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Home</title>
+		  <link rel="stylesheet" href="style.css">
+		  <link rel="icon" type="image/x-icon" href="resources/images/favicon.ico">
+		  <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>
+	
+	<div id="navbar">
+	<ul>
+	  <li style="float:right"><a class="navi, navi1" href="portfolio.html">Portfolio</a></li>
+	  <li style="float:right"><a class="navi" href="mystory.html">My Story</a></li>
+	  <li style="float:right"><a class="navi" href="index.html">Home</a></li>
+	  <li><a class="active" href="index.html">WanderWithWayne.com</a></li>
+	</ul>
+	</div>
+	
+	<img src="resources/images/headingclouds.png" id="header">
+	<img src="resources/images/collage.png" id="collage">
+	<img src="resources/images/tagline.png" id="tagline">
+	<img src="resources/images/footer.png" id="footer">
+	
+	<a target="_blank" href="https://www.facebook.com/dzn.wayne">
+	<img src="resources/images/facebook.png" id="facebook"></a>
+	
+	<a target="_blank" href="https://www.linkedin.com/in/wayne-denise-dizon-0b89b7248/">
+	<img src="resources/images/linkedin.png" id="linkedin"></a>
+	
+	<img src="resources/images/heartfoot.gif" id="heartfoot">
+	
+	<a target="_blank" href="https://www.instagram.com/wayne_dzn?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr">
+	<img src="resources/images/instagram.png" id="instagram"> </a>
+	
+	
+
+	<h2 id="js1"></h2>
+	
+	<h3> Have a question or <br> wanna collab? </h3>
+	
+	<a href="index.html"><h4>HOME</h4></a>
+	<a href="mystory.html"><h5>MY STORY</h5></a>
+	
+	<div class="tooltip">RESOURCES
+	<span class="tooltiptext"> Source Code: W3 Schools, Khan Academy<br> Pictures: Pinterest, Canva</span>
+	</div>
+	
+	
+		<!--
+		<p style="text-align:center;"> My name is <strong><span class="note">Wayne Denise Dizon</span></strong>, but I go by Wayne. <br>
+		I'm 19 years old and was born on March 21, 2004. <br>
+		I graduated from <span class="note"><em>Makati Science High School</em></span>, located in Makati City. <br>
+		Currently, I'm furthering my education by pursuing a <strong><span class="note"><abbr title="Bachelor of Science in Information Technology">BSIT</abbr></span></strong>, <br>
+		specializing in Mobile and Internet Technology. <br>
+		This field ignites my passion as I'm eager to establish myself in the dynamic realm of technology.<br>
+		</p>
+		-->
+		
+
+		
+	<!-- Button -->	
+		<!--button onclick="myFunction()">Hide TMI</button-->
+		
+	<!-- Contact Me -->
+		<!--<h3 style="text-align:center;"> Facebook <br> Instagram <br> </h3>
+	
+
+	<!-- Button Function -->
+	<script src="script.js">
+	
+	
+	
+	</script>
+	<!--
+	function myFunction() {
+	  var x = document.getElementsByClassName("city");
+	  for (var i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	  }
+	}
+	-->
+    </body>
+</html>
---
+++ week8/portfolio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Portfolio</title>
+		  <link rel="stylesheet" href="style.css">
+		  <link rel="icon" type="image/x-icon" href="resources/images/favicon.ico">
+		  <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>	
+
+		<button id="btn">Click Me!</button>
+		<p id="demo"></p>
+		<p id="demo"></p>
+		<p>Wait 3 seconds (3000 milliseconds) for this page to change.</p>
+		<h1 id="demo"></h1>
+		
+		<p><button onclick="moveSquare()">Move Me</button></p> 
+
+		<div id="container">
+		<div id="animate"></div>
+		</div>
+
+		<h2>JavaScript Prompt</h2>
+
+		<button onclick="myFunction()">Like my website?</button>
+
+		<p id="demo"></p>
+		
+	<script src="script.js">
+	</script>
+	
+	</body>
+</html>
