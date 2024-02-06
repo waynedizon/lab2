381522f17861a9fc151f4490a4b2640c1985e0a7
Wayne Dizon <122617930+waynedizon@users.noreply.github.com>
2024-02-06 08:29:50 +0800

    Update form.php

--- week8/form.php
+++ week8/form.php
@@ -100,19 +100,39 @@ echo "<br>";
 echo $gender;
 ?>
 
-
+<?php
+// For Xampp Localhost
 //$servername = "localhost";
 //$username = "root";
 //$password = "";
 //$dbname = "myDB";
 
-  
-<?php
+// For socitcloud
 $servername = "localhost";
 $username = "webprogmi222_sf221";
 $password = "xE*Y2nleNVvZm[!!";
 $dbname = "webprogmi222_sf221";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO jlquesada_myguests (name, email,website,comment,gender)
+VALUES ('$name', '$email','$website','$comment','$gender')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
 
+  
 </body>
 </html>
