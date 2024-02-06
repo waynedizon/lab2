37ead6b84cd37e363afa976302f45d891fe4c4e8
Wayne Denise Dizon <wddizon@student.apc.edu.ph>
2024-02-06 08:45:51 +0800

    upload selectdata.php

--- week8/form.php
+++ week8/form.php
@@ -100,5 +100,30 @@ echo "<br>";
 echo $gender;
 ?>
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (name, email, website, comment, gender)
+VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
 </body>
 </html>
